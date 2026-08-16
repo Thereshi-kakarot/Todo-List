51fddab2bcd426147516f04360219aa16e88ae36
root <root@Johnny.>
2026-08-16 23:45:19 +0200

    I have added the dialog and the form for projects and todos entries

--- src/index.js
+++ src/index.js
@@ -5,11 +5,3 @@ import { state } from "./modules/state.js";
 
 const projects = [];
 
-const myProject = createProject('Default Project');
-projects.push(myProject);
-
-const todo = createTodo('Task 1', 'Description 1', '2023-12-31', 'High');
-
-myProject.add(todo);
-projects.push(myProject);
-console.log(projects);
