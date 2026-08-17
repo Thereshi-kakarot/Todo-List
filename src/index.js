b0e85037394a6de2817692e7ffa77e0db37b2717
root <root@Johnny.>
2026-08-17 22:29:55 +0200

    I have made initail DOM confirmation plus styling of modal dialog

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import './style.css'
+import './style.css';
+import './modules/ui.js';
 import createProject from "./modules/project.js";
 import { createTodo } from "./modules/todos.js";
 import { state } from "./modules/state.js";
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -3,3 +3,8 @@ const addTodoBtn = document.getElementById("add-todo");
 const delProjectBtn = document.getElementById("del-project");
 const container = document.getElementById("container");
 const project = document.getElementById("project");
+const projectModal = document.getElementById("project-modal");
+
+addProjectBtn.addEventListener("click", ()=> {
+    projectModal.style.display = "block";
+});
