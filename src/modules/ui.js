045bfa01c8a2c526e626b8361de14c9f93151f1c
root <root@Johnny.>
2026-08-18 22:33:53 +0200

    Modified the project div, and added some UI functionality

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,10 +1,48 @@
+import createProject from "./project.js";
+import { state } from "./state.js";
+
+document.addEventListener("DOMContentLoaded", () => {
 const addProjectBtn = document.getElementById("add-project");
 const addTodoBtn = document.getElementById("add-todo");
 const delProjectBtn = document.getElementById("del-project");
 const container = document.getElementById("container");
 const project = document.getElementById("project");
 const projectModal = document.getElementById("project-modal");
+const closeProjectModalBtn = document.getElementById("close-project-modal-btn");
+const addProjectModalBtn = document.getElementById("add-project-modal-btn");
+const projectTitle = document.querySelector("#project-title");
+const asideContainer = document.getElementById("aside-container");
+
+
 
 addProjectBtn.addEventListener("click", ()=> {
     projectModal.style.display = "block";
 });
+
+closeProjectModalBtn.addEventListener("click", ()=> {
+    projectModal.style.display = "none";
+});
+
+addProjectModalBtn.addEventListener("click", ()=> {
+
+    
+    if(projectTitle.value.trim() === ""){
+        alert("Please enter a project title");
+        return;
+    }
+    
+    const newProject = createProject(projectTitle.value.trim());
+    state.projects.push(newProject);
+
+    projectModal.style.display = "none";
+    projectTitle.value = "";
+
+    asideContainer.innerHTML = "";
+    state.projects.forEach((project, index)=> {
+        
+    });
+});
+});
+
+
+
